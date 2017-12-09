b153a49f5be823f80741ce97adca1371f3be3fa5
Colin Kiama <colinkiama@hotmail.co.uk>
2017-12-09 21:59:36 +0000

    uggghhhh so close!!

--- Flames/shell.xaml.cs
+++ Flames/shell.xaml.cs
@@ -5,15 +5,18 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
@@ -26,39 +29,142 @@ namespace Flames
     /// </summary>
     public sealed partial class shell : Page
     {
+
         int characterOffset = 0;
         bool isMovingRight = false;
         bool isMovingLeft = false;
         bool isDead = false;
+        bool gameLoopEnabled = true;
         public shell()
         {
             this.InitializeComponent();
             characterOffset = 0;
+            App.Current.DebugSettings.EnableFrameRateCounter = true;
 
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            startCreatingEnemies();
+            Random rnd = new Random();
+
+
+            while (gameLoopEnabled)
+            {
+                float randomPosX = rnd.Next(0, (int)Window.Current.Bounds.Width - 20);
+                await startCreatingEnemies(randomPosX);
+            }
+
         }
 
-        private async void startCreatingEnemies()
+        private void drawFrame()
         {
-            var circleEnemy = new Ellipse();
-            circleEnemy.Fill = new SolidColorBrush(Colors.Red);
-            circleEnemy.Width = 20;
-            circleEnemy.Height = 20;
-            circleEnemy.HorizontalAlignment = HorizontalAlignment.Left;
-            circleEnemy.VerticalAlignment = VerticalAlignment.Top;
-            gameScreen.Children.Add(circleEnemy);
-            do
+
+        }
+
+        private void updateFrame()
+        {
+
+        }
+
+        private async Task startCreatingEnemies(float randomPosX)
+        {
+            await Task.Delay(100);
+            //var circleEnemy = new Ellipse();
+            //circleEnemy.Fill = new SolidColorBrush(Colors.Red);
+            double circleSize = 100;
+            var circleEnemy = new flame(200, randomPosX);
+
+            circleEnemy.flameImage.HorizontalAlignment = HorizontalAlignment.Left;
+            circleEnemy.flameImage.VerticalAlignment = VerticalAlignment.Top;
+            flame.flames.Add(circleEnemy);
+            await circleEnemy.flameImage.Offset(randomPosX, -500, 0).StartAsync();
+            gameScreen.Children.Add(circleEnemy.flameImage);
+
+
+            var anim = circleEnemy.flameImage.Offset(randomPosX, (float)(gameScreen.ActualHeight - circleEnemy.flameImage.Height - mainCharacter.ActualHeight), 300, 0, EasingType.Sine);
+
+            anim.Completed += Anim_Completed;
+            circleEnemy.isMoving = true;
+
+
+            await anim.StartAsync();
+
+        }
+
+        private async Task<bool> checkIfHitDetected(Image flame)
+        {
+            bool wtf = false;
+           
+                var flameVisual = flame.TransformToVisual(gameScreen);
+                var flamePoint = flameVisual.TransformPoint(new Point(0, 0));
+                var flamePointX = flamePoint.X;
+                var actualFlameWidth = flame.ActualWidth;
+
+                var mainCharVisual = mainCharacter.TransformToVisual(gameScreen);
+                var mainCharPoint = mainCharVisual.TransformPoint(new Point(0, 0));
+                var mainCharPointX = mainCharPoint.X;
+                var actualMainCharWidth = mainCharacter.ActualWidth;
+               
+                await Task.Run(() =>
+                {
+                    for (int i = (int)flamePointX; i < flamePointX + actualFlameWidth + 1; i++)
+                    {
+                        for (int j = (int)mainCharPointX; j < mainCharPointX + actualMainCharWidth; j++)
+                        {
+                            if (i == j)
+                            {
+                                Debug.WriteLine("GAME OVER YEAAAAAAAH");
+                                wtf = true;
+                            }
+
+                        }
+                        if (i == flamePointX + actualFlameWidth)
+                        {
+                            Debug.WriteLine("WTF");
+                        }
+                    }
+                });
+                
+
+           
+
+            return wtf;
+
+
+
+        }
+
+        private async void Anim_Completed(object sender, AnimationSetCompletedEventArgs e)
+        {
+
+            var anim = (AnimationSet)sender;
+            var flame = (Image)anim.Element;
+
+            var thisCircleEnemy = Flames.flame.flames.First();
+            bool isHit = await checkIfHitDetected(flame);
+            if (!isHit)
             {
-                await circleEnemy.Offset(100).StartAsync();
-                await circleEnemy.Offset(0).StartAsync();
-            } while (isDead == false);
+                var fadeAnim = flame.Offset(thisCircleEnemy.animXOffset, (float)(gameScreen.ActualHeight - flame.ActualHeight));
+                fadeAnim.Completed += FadeAnim_Completed;
+               await fadeAnim.StartAsync();
+            }
+            else { 
+            gameLoopEnabled = false;
+           
+            }
 
+        }
 
+        private void FadeAnim_Completed(object sender, AnimationSetCompletedEventArgs e)
+        {
+            var anim = (AnimationSet)sender;
+            var flame = (Image)anim.Element;
+            gameScreen.Children.Remove(flame);
+
+            var thisCircleEnemy = Flames.flame.flames.First();
+            Flames.flame.flames.Remove(thisCircleEnemy);
         }
 
         private async void Page_KeyDown(object sender, KeyRoutedEventArgs e)
@@ -73,12 +179,12 @@ namespace Flames
                         if (characterOffset + 10 < gameScreen.ActualWidth - 20)
                         {
 
-                        characterOffset += 10;
-                        await mainCharacter.Offset(characterOffset, duration: 0).StartAsync();
+                            characterOffset += 10;
+                            await mainCharacter.Offset(characterOffset, duration: 0).StartAsync();
                         }
                         else if (characterOffset < gameScreen.ActualWidth - 20)
                         {
-                            await mainCharacter.Offset((float)gameScreen.ActualWidth -20, duration: 0).StartAsync();
+                            await mainCharacter.Offset((float)gameScreen.ActualWidth - 20, duration: 0).StartAsync();
                             isMovingRight = false;
                         }
                         else
@@ -101,7 +207,7 @@ namespace Flames
                         {
                             characterOffset -= 10;
                             await mainCharacter.Offset(characterOffset, duration: 0).StartAsync();
-                            
+
                         }
                         else if (characterOffset > 0)
                         {
@@ -116,7 +222,7 @@ namespace Flames
                     }
                 }
             }
-            
+
         }
 
         private async void Page_KeyUp(object sender, KeyRoutedEventArgs e)
